d97cba633556343d54cee544f47c785139bf0e65
josiah-williams2024 <josiah.williams.dev@gmail.com>
2026-05-24 15:07:20 -0400

    first commit

---
+++ resources/js/components/AppLogo.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import AppLogoIcon from '@/components/AppLogoIcon.vue';
+</script>
+
+<template>
+    <div
+        class="flex aspect-square size-8 items-center justify-center rounded-md bg-sidebar-primary text-sidebar-primary-foreground"
+    >
+        <AppLogoIcon class="size-5 fill-current text-white dark:text-black" />
+    </div>
+    <div class="ml-1 grid flex-1 text-left text-sm">
+        <span
+            class="hidden text-2xl leading-none font-bold tracking-wider text-gray-600 uppercase lg:block">
+            Vehikl Mob Planner
+        </span>
+    </div>
+</template>
---
+++ resources/js/components/AppSidebar.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import { Link } from '@inertiajs/vue3';
+
+import AppLogo from '@/components/AppLogo.vue';
+
+import NavUser from '@/components/NavUser.vue';
+import {
+    Sidebar,
+    SidebarContent,
+    SidebarFooter,
+    SidebarHeader,
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem,
+} from '@/components/ui/sidebar';
+import { dashboard } from '@/routes';
+
+
+
+
+
+</script>
+
+<template>
+    <Sidebar collapsible="icon" variant="inset">
+        <SidebarHeader>
+            <SidebarMenu>
+                <SidebarMenuItem>
+                    <SidebarMenuButton size="lg" as-child>
+                        <Link :href="dashboard()">
+                            <AppLogo />
+                        </Link>
+                    </SidebarMenuButton>
+                </SidebarMenuItem>
+            </SidebarMenu>
+        </SidebarHeader>
+
+        <SidebarContent>
+
+        </SidebarContent>
+
+        <SidebarFooter>
+
+            <NavUser />
+        </SidebarFooter>
+    </Sidebar>
+    <slot />
+</template>
